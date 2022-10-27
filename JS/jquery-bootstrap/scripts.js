0d0493afd3d076dc9570f3b9fbcd15bdce3c0005
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-10-27 19:16:44 -0300

    JQuery Bootstrap Introducao

--- JS/jquery-bootstrap/scripts.js
+++ JS/jquery-bootstrap/scripts.js
@@ -4,7 +4,21 @@ $(document).ready((e)=>{
 
     $('#btnSalvar').click((e)=>{
         e.preventDefault();
-        console.log(`Nome: ${$('#nome').val()}`);
+        var nome = $('#nome');
+        var email = $('#email');
+        // console.log(`Nome: ${$('#nome').val()}`);
+
+        console.log(nome.val());
+        console.log(email);
+        if(nome.val() === "" ||
+           email.val() === ""){
+            $('#form').css('border', '2px solid red');
+        } else {
+            $('#form').css('border', '2px solid green');
+            $('#nome-quadro').html(nome.val());
+            $('#email-quadro').html(email.val());
+            $('#quadro').show();
+        }
     });
 
     $('#email').on('change', (e)=>{
@@ -20,9 +34,23 @@ $(document).ready((e)=>{
 
             if(valid){
                 console.log('Email válido');
+                $('#email').css({
+                    'background-color':'#ccc'
+                });    
+                $('#nome').css({
+                    'background-color':'#ccc'
+                });    
             } else {
                 console.log('Email inválido');
-                e.target.focus();           
+                e.target.focus();   
+                $('#email').css('outline','1px solid red');      
             }
+    });
+
+    $('#quadro').on('mouseenter', (e)=>{
+        $('#quadro').addClass('animacao-quadro')
+    });
+    $('#quadro').on('mouseleave', (e)=>{
+        $('#quadro').removeClass('animacao-quadro')
     });
 });
