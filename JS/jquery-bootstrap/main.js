7a4ff5f6962066fb7e4424da033ca1482a70a1cc
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-10-27 20:40:08 -0300

    Introducao Bootstrap

--- JS/jquery-bootstrap/main.js
+++ JS/jquery-bootstrap/main.js
@@ -4,4 +4,17 @@ $(document).ready(()=>{
     $('#btn-login').click((e)=>{
         $('#modalLogin').modal('toggle');
     })
+
+    // Carregar Pagina
+    $('#intro-jquery').click((e)=>{
+        $.ajax({
+            url: "introducao-jquery.html",
+            success: (data, textStatus, xhr)=>{
+                console.log(data);
+                console.log(textStatus);
+                console.log(xhr);
+                $('#conteudo').html(data);
+            }
+          });
+    });
 });
