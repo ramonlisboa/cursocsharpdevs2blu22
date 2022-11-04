98e3263ac541218e307bb2627921e6f3e51cb809
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-04 20:00:57 -0300

    JQuery Bootstrap - Consumir API

---
+++ JS/consumir-api/_js/library.js
@@ -0,0 +1,25 @@
+/* # Confs */
+
+
+
+/* # Functions*/
+const getPagina = () => {
+
+}
+
+
+
+/*
+- Function
+function myFunc() {
+
+}
+
+- Arrrow Function
+const myFunc = () => {}
+
+- Anon Function
+setTimeout(()=>{}, 500)
+setTimeout(function()=>{}, 500)
+
+*/
---
+++ JS/consumir-api/_js/main.js
@@ -0,0 +1,10 @@
+/* # Confs */
+const URL_API = 'https://pokeapi.co/api/v2/pokemon/';
+
+$(document).ready(()=>{
+
+})
+
+const getPokemonList = (offset=null,limit=null) => {
+    
+}
