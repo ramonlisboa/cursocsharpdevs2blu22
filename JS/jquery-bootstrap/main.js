ff447ebd8e8eeb840ca62df3befdf6df83101a40
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-08 19:13:20 -0300

    JQuery Bootstrap - Msg Toast TimeOut

--- JS/jquery-bootstrap/main.js
+++ JS/jquery-bootstrap/main.js
@@ -1,6 +1,8 @@
 $(document).ready(()=>{
     console.log('JQuery is loaded.');
 
+    $('#alert').hide();
+
     $('#btn-login').click((e)=>{
         $('#modalLogin').modal('toggle');
     })
@@ -17,4 +19,29 @@ $(document).ready(()=>{
             }
           });
     });
+
+    $('#btn-salvar-teste').click(()=> {
+        let nome = $('#nome').val();
+        let idade = $('#idade').val();
+        let htmlMsg = `Pessoa ${nome} - Idade ${idade}`;
+        showMsgToast(htmlMsg);
+        showMsgAlert(htmlMsg);
+    });
 });
+
+
+const showMsgToast = (msg) => {
+    let elToast = document.getElementById('msgToast');
+    $('#msg-body').html(msg);
+    const toast = new bootstrap.Toast(elToast)
+    toast.show();
+}
+
+const showMsgAlert = (msg) => {
+    $('#msg-alert').html(msg);
+    $('#alert').fadeIn('slow');
+
+    setTimeout(()=>{
+        $('#alert').fadeOut('slow');
+    },3000);
+}
