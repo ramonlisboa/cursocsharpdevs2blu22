f8c92691c8d540374fee8fb0ebbdf6f479df24e5
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-11 20:18:36 -0300

    Javascript - Revisao 2 Pagination

--- JS/revisao2-js/_js/main.js
+++ JS/revisao2-js/_js/main.js
@@ -9,9 +9,10 @@ const criaListaCharacter = (data) => {
     let main = getElement('main');
     console.log(data);
     listCharacter = new Array();
+
     data.results.forEach(character => {
         let html = document.createElement('div');
-        html.classList.add('card', 'col-4', 'my-4', 'bg-dark');
+        html.classList.add('card', 'col-2', 'my-4', 'bg-dark', 'ms-1');
         html.addEventListener('click', ()=> mostraDetalhesCharacter(character))
 
         let htmlBody = `
@@ -19,13 +20,15 @@ const criaListaCharacter = (data) => {
             <img class="card-img-top" src="${character.image}" alt="${character.name}">
         </div>
         <div class="card-body bg-white">
-            <h1 class="text-primary text-center">${character.name}</h1>
+            <h2 class="text-primary text-center">${character.name}</h2>
         </div>`;
 
         html.innerHTML = htmlBody;
         main.appendChild(html);
         listCharacter.push(character);
     });
+
+    gerarPaginacao(data.info);
 }
 
 const mostraDetalhesCharacter = (character) => {
@@ -58,3 +61,55 @@ const mostraDetalhesCharacter = (character) => {
 
         $('#charModal').modal('show');
 }
+
+const gerarPaginacao = (info, currentPage=null) => {
+    let pag = getElement('#pagination');
+    pag.innerHTML = "";
+    
+    let prevItem = novoItemPagination(info.prev, 'Previous');
+    pag.appendChild(prevItem);
+    
+    let indexPagination = (currentPage && currentPage > 5) ? currentPage - 5 : 0;
+
+    for (let index = indexPagination; index < info.pages; index++) {
+        if(currentPage == null &&
+             index < 5) {
+            let liItemPag = novoItemPagination(`${URL_API_CHARACTER}?page=${index + 1}`, `${index + 1}`);
+                pag.appendChild(liItemPag); 
+        }
+        if(currentPage == null &&
+            (index > (info.pages - 5) &&
+                index < info.pages)) {
+           let liItemPag = novoItemPagination(`${URL_API_CHARACTER}?page=${index + 1}`, `${index + 1}`);
+               pag.appendChild(liItemPag); 
+       }
+
+        // item clicado
+        if(currentPage && !(indexPagination > (currentPage) ||
+            indexPagination < (info.pages - 5))) {
+                let liItemPag = novoItemPagination(`${URL_API_CHARACTER}?page=${index + 1}`, `${index + 1}`);
+                pag.appendChild(liItemPag);                
+        }
+    }
+
+    let nextItem = novoItemPagination(info.next, 'Next');
+    pag.appendChild(nextItem);
+}
+
+const novoItemPagination = (url, index) => {
+    let liItem = document.createElement('li');
+    liItem.classList.add('page-item');    
+    liItem.innerHTML = `<a onclick="irItemPaginacao('${url}')" class="page-link" href="#">${index}</a>`;
+    return liItem;
+}
+
+const irItemPaginacao = (url) => {
+
+}
+
+let info = {
+    count: 826,
+    pages: 42,
+    next: "https://rickandmortyapi.com/api/character?page=2",
+    prev: null
+  }
