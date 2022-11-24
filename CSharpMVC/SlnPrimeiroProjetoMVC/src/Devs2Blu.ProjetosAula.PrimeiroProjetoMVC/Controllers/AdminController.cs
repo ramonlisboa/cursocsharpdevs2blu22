8bd5f42cfacea544ebd190a49d4b4ec2f61dc716
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-24 20:37:42 -0300

    C# MVC Introducao

---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/src/Devs2Blu.ProjetosAula.PrimeiroProjetoMVC/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetosAula.PrimeiroProjetoMVC.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("indicadores")]    
+        [Route("numbers")]    
+        public IActionResult Indicadores()
+        {
+            return View();
+        }
+    }
+}
