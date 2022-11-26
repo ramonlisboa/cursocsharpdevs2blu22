eb7b23e04c2e4d3819fe707ac6a685bf9833a92f
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-25 22:20:40 -0300

    C# MVC - Model Service

---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/src/Devs2Blu.ProjetosAula.PrimeiroProjetoMVC/Models/Pokemons.cs
@@ -0,0 +1,16 @@
+﻿namespace Devs2Blu.ProjetosAula.PrimeiroProjetoMVC.Models
+{
+    public class Pokemons
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<ResultsPokemons> results { get; set; }
+    }
+
+    public class ResultsPokemons
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
