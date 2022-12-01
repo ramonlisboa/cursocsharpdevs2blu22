4a8fe00b8bb0a4df389fe791b161760e2ce682ae
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-11-30 21:55:38 -0300

    C# MVC - Revisao API

---
+++ CSharpMVC/SlnRevisaoCSharpMVC/src/Devs2Blu.ProjetosAula.CSharpMVCRevisao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devs2Blu.ProjetosAula.CSharpMVCRevisao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CSharpMVC/SlnRevisaoCSharpMVC/src/Devs2Blu.ProjetosAula.CSharpMVCRevisao/Services/ServiceAPI.cs
@@ -0,0 +1,71 @@
+﻿using Devs2Blu.ProjetosAula.CSharpMVCRevisao.Models.DTOAPI;
+using System.Net.Http;
+
+namespace Devs2Blu.ProjetosAula.CSharpMVCRevisao.Services
+{
+    public class ServiceAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public ServiceAPI()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<CardDTO>> GetListCards()
+        {
+            var objJSONResponse = await Get<GetListDataCardsDTO>(URL_API);
+            var listCards = objJSONResponse.Data;
+            return listCards;
+        }
+
+        public async Task<CardDTO> GetCardByName(string name)
+        {
+            var urlSerach = $"{URL_API}?name={name}";
+            return await Get<CardDTO>(urlSerach);
+        }
+
+
+        #region BaseMethods
+
+        public async Task<T> Get<T>(string url)
+        {
+            var objHttp = await GetAsync(url);
+
+            if (!objHttp.IsSuccessStatusCode)
+                return (T)(object)url;
+
+            return await objHttp.Content.ReadFromJsonAsync<T>();
+
+        }
+
+        public async Task<List<T>> GetList<T>(string url)
+        {
+            var listHttp = await GetAsync(url);
+
+            if (!listHttp.IsSuccessStatusCode)
+                return new List<T>();
+
+            return await listHttp.Content.ReadFromJsonAsync<List<T>>();
+
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var getRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url)
+            };
+            return await _httpClient.SendAsync(getRequest);
+        }
+        #endregion
+
+
+        #region CONSTS
+
+        private const string URL_API = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+
+        #endregion
+    }
+}
