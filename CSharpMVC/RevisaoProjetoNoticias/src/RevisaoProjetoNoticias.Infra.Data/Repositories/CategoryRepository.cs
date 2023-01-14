f61dc93e30c3f9c1c67b2d916c4e116fbaf6fe64
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2023-01-13 22:10:39 -0300

    Repositories + Data

---
+++ CSharpMVC/RevisaoProjetoNoticias/src/RevisaoProjetoNoticias.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using RevisaoProjetoNoticias.Domain.Entities;
+using RevisaoProjetoNoticias.Domain.IRepositories;
+using RevisaoProjetoNoticias.Infra.Data.Context;
+
+namespace RevisaoProjetoNoticias.Infra.Data.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        private readonly SQLServerContext _context;
+        public CategoryRepository(SQLServerContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ CSharpMVC/RevisaoProjetoNoticias/src/RevisaoProjetoNoticias.Infra.Data/Repositories/NewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using RevisaoProjetoNoticias.Domain.Entities;
+using RevisaoProjetoNoticias.Domain.IRepositories;
+using RevisaoProjetoNoticias.Infra.Data.Context;
+
+namespace RevisaoProjetoNoticias.Infra.Data.Repositories
+{
+    public class NewsRepository : BaseRepository<News>, INewsRepository
+    {
+        private readonly SQLServerContext _context;
+        public NewsRepository(SQLServerContext context) : base(context)
+        {
+        }
+    }
+}
