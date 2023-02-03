b3eb3cfa31d12e7309c07ee732c7ffc8ab1143de
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2023-02-03 20:21:12 -0300

    WEBAPI Projeto 1 + Revisao

---
+++ CSharpAPI/PrimeiraAPIASPNetCore/src/Devs2Blu.PrimeiraAPIASPNetCore.API/Models/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Devs2Blu.PrimeiraAPIASPNetCore.API.Models.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Contact1 { get; set; }
+        public string? Contact2 { get; set; }
+        public string? Email { get; set; }
+    }
+}
