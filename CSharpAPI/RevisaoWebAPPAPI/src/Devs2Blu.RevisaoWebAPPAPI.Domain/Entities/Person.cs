f58eb6de9c5b9ae9021f8717ee6605a469e4c067
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2023-02-06 09:23:29 -0300

    Revisao DDD + API

---
+++ CSharpAPI/RevisaoWebAPPAPI/src/Devs2Blu.RevisaoWebAPPAPI.Domain/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoWebAPPAPI.Domain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public DateTime Birth { get; set; }
+    }
+}
