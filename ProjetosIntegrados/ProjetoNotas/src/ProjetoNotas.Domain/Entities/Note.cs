43fbe60d1d259fe852a2aa294d5595007f0233f3
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2023-02-24 22:04:18 -0300

    Fase 1 - Planning

---
+++ ProjetosIntegrados/ProjetoNotas/src/ProjetoNotas.Domain/Entities/Note.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoNotas.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNotas.Domain.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CategoryEnum Category { get; set; }
+        public bool Fixed { get; set; }
+        public DateTime TimeNote { get; set; }
+
+        public virtual User? User { get;set; }
+    }
+}
---
+++ ProjetosIntegrados/ProjetoNotas/src/ProjetoNotas.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNotas.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Note>? Notes { get; set;}
+    }
+}
